8539131177773a17515ebf47ba09ba4fc0e272d1
Tate Yeung <yeungtate@gmail.com>
2017-11-28 18:05:17 -0800

    contact

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "p@pcccki.org";
+        $recipient = "gowest@pcccki.org";
 
         // Set the email subject.
         $subject = "gowest.pcccki.org message from $name";
